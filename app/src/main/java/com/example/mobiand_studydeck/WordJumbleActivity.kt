9dc5af8d1950470559fec7db17d4c5c4790f2530
Jairus Macatangay <jai.macatangay@gmail.com>
2021-04-21 19:47:17 +0800

    Word Jumble Module - Retrieve data from database and fixed some bugs

--- app/src/main/java/com/example/mobiand_studydeck/WordJumbleActivity.kt
+++ app/src/main/java/com/example/mobiand_studydeck/WordJumbleActivity.kt
@@ -2,17 +2,22 @@ package com.example.mobiand_studydeck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.example.mobiand_studydeck.databinding.ActivityWordJumbleBinding
-import com.example.mobiand_studydeck.viewModel.WordJumbleViewModel
+import com.example.mobiand_studydeck.flashcards.DatabaseHandler
+import com.example.mobiand_studydeck.flashcards.FlashcardsModelClass
 import com.google.android.material.snackbar.Snackbar
 
 class WordJumbleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWordJumbleBinding
+    private lateinit var arrayWordJumble: Array<String?>
+    private lateinit var jumbledword: String
+    private lateinit var correctWord: String
     private var totalJumbledWords: Int = 0
     private var enteredWord: String = ""
-    private var firstLetter: Char = 'a'
-    private var lastLetter: Char = 'a'
+    private var jumbledWordCurrentIndex: Int = 1
+    private var snackbarEvent: Boolean = false
+    private var firstLetterOfJumbledWord: Char = 'a'
+    private var lastLetterOfJumbledWord: Char = 'z'
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,57 +25,123 @@ class WordJumbleActivity : AppCompatActivity() {
         binding = ActivityWordJumbleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewModel by viewModels<WordJumbleViewModel>()
+        arrayWordJumble = getTermsArray()
+        totalJumbledWords = savedInstanceState?.getInt(TOTAL_JUMBLED_WORDS.toString())
+                ?: arrayWordJumble.size
+        firstLetterOfJumbledWord = savedInstanceState?.getChar(FIRST_LETTER.toString()) ?: 'a'
+        lastLetterOfJumbledWord = savedInstanceState?.getChar(LAST_LETTER.toString()) ?: 'b'
+        jumbledWordCurrentIndex = savedInstanceState?.getInt(JUMBLED_WORD_INDEX.toString()) ?: 1
+        jumbledword = (savedInstanceState?.getString(JUMBLED_WORD)
+                ?: arrayWordJumble[0]?.toUpperCase()) as String
+        correctWord = (savedInstanceState?.get(CORRECT_WORD)
+                ?: arrayWordJumble[0]?.toUpperCase()) as String
 
-        viewModel.totalJumbledWords.observe(this) {
-            totalJumbledWords = it
-        }
+        shuffleWord()
+        displayValues()
 
-        viewModel.firstLetterOfJumbledWord.observe(this) {
-            firstLetter = it
-        }
-
-        viewModel.lastLetterOfJumbledWord.observe(this) {
-            lastLetter = it
-        }
-
-        viewModel.jumbledWordCurrentIndex.observe(this){
-            binding.txtCount.text = "$it / $totalJumbledWords"
-        }
-
-        viewModel.jumbledword.observe(this){
-            binding.txtWord.text = it
-        }
-
-        binding.btnCheck.setOnClickListener{
+        binding.btnCheck.setOnClickListener {
             enteredWord = binding.txtAnswer.text.toString()
-            viewModel.checkIfMatch(enteredWord.toUpperCase())
+            checkIfMatch(enteredWord.toUpperCase())
 
-            viewModel.snackbarEvent.observe(this){
-                if (it == true){
-                    val snack = Snackbar.make(binding.root, "You got it!", Snackbar.LENGTH_SHORT)
-                    snack.show()
-                    binding.txtAnswer.text.clear()
-                } else {
-                    val snack = Snackbar.make(binding.root, "Try again!", Snackbar.LENGTH_SHORT)
-                    snack.show()
-                }
+            if (snackbarEvent) {
+                Snackbar.make(binding.root, "You got it!", Snackbar.LENGTH_SHORT).show()
+                binding.txtAnswer.text.clear()
+            } else {
+                Snackbar.make(binding.root, "Try again!", Snackbar.LENGTH_SHORT).show()
             }
         }
 
         binding.btnNext.setOnClickListener{
-            viewModel.nextWord()
+            nextWord()
             binding.txtAnswer.text.clear()
         }
 
         binding.btnBack.setOnClickListener{
-            viewModel.previousWord()
+            previousWord()
             binding.txtAnswer.text.clear()
         }
 
         binding.btnHint.setOnClickListener{
-            val snack = Snackbar.make(binding.root, "First letter is $firstLetter, and last letter is $lastLetter.", Snackbar.LENGTH_SHORT)
-            snack.show()
+            Snackbar.make(binding.root, "First letter is $firstLetterOfJumbledWord, and " +
+                    "last letter is $lastLetterOfJumbledWord.", Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    private fun getFlashcards(): ArrayList<FlashcardsModelClass> {
+        val dbHandler: DatabaseHandler = DatabaseHandler(this)
+        return dbHandler.viewFlashcard()
+    }
+
+    private fun append(arr: Array<String?>, element: String): Array<String?> {
+        val array = arr.copyOf(arr.size + 1)
+        array[arr.size] = element
+        return array
+    }
+
+    private fun getTermsArray(): Array<String?> {
+        var flashcardsList: ArrayList<FlashcardsModelClass> = getFlashcards()
+        var arrayFlashcards = emptyArray<String?>()
+
+        for (item in flashcardsList) {
+            var term = item.term
+            arrayFlashcards = append(arrayFlashcards, term)
+        }
+
+        return arrayFlashcards
+    }
+
+    private fun checkIfMatch(answer: String){
+        var correctAnswer: String = arrayWordJumble[jumbledWordCurrentIndex - 1]?.toUpperCase()
+                ?: "Default"
+        snackbarEvent = correctAnswer == answer
+    }
+
+    private fun shuffleWord(){
+        val usedWord = correctWord.toCharArray()
+        firstLetterOfJumbledWord = usedWord[0]
+        lastLetterOfJumbledWord = usedWord[usedWord.size - 1]
+        val shuffledWord = StringBuilder()
+        usedWord.shuffle()
+
+        for (letter in usedWord) {
+            shuffledWord.append(letter)
+        }
+
+        jumbledword = shuffledWord.toString()
+    }
+
+    private fun nextWord(){
+        if (jumbledWordCurrentIndex < totalJumbledWords){
+            jumbledWordCurrentIndex += 1
+            loadJumbledWord()
+        }
+        displayValues()
+    }
+
+    private fun previousWord(){
+        if (jumbledWordCurrentIndex > 1){
+            jumbledWordCurrentIndex -= 1
+            loadJumbledWord()
+        }
+        displayValues()
+    }
+
+    private fun displayValues() {
+        binding.txtCount.text = "$jumbledWordCurrentIndex / $totalJumbledWords"
+        binding.txtWord.text = jumbledword
+    }
+
+    private fun loadJumbledWord(){
+        correctWord = arrayWordJumble[jumbledWordCurrentIndex - 1]?.toUpperCase() ?: "Default"
+        shuffleWord()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(TOTAL_JUMBLED_WORDS.toString(), totalJumbledWords)
+        outState.putChar(FIRST_LETTER.toString(), firstLetterOfJumbledWord)
+        outState.putChar(LAST_LETTER.toString(), lastLetterOfJumbledWord)
+        outState.putInt(JUMBLED_WORD_INDEX.toString(), jumbledWordCurrentIndex)
+        outState.putString(JUMBLED_WORD, jumbledword)
+        super.onSaveInstanceState(outState)
     }
 }
---
+++ app/src/main/java/com/example/mobiand_studydeck/WordJumbleConstants.kt
@@ -0,0 +1,8 @@
+package com.example.mobiand_studydeck
+
+const val TOTAL_JUMBLED_WORDS = 0
+const val FIRST_LETTER = 'a'
+const val LAST_LETTER = 'z'
+const val JUMBLED_WORD_INDEX = 1
+const val JUMBLED_WORD = "Default"
+const val CORRECT_WORD = "Default"
